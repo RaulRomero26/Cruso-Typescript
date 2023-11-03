7558ddef92a96879cf13f0c44758728e38e9a133
RaulRomero26 <link_2642@hotmail.com>
2023-11-03 17:51:17 -0600

    update: objetos y tipos

---
+++ bases/objetos/type.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: [1, 2]
+    };
+    let superman = {
+        name: 'Clack Kent',
+        age: 60,
+        powers: [1]
+    };
+})();
