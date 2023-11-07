8500d4b5e81bcbf11398f6d66730706e3bbc8dd9
RaulRomero26 <link_2642@hotmail.com>
2023-11-07 17:59:06 -0600

    update: tipos de propiedades

--- bases/clases/basica.ts
+++ bases/clases/basica.ts
@@ -2,22 +2,32 @@
 
     class Avenger {
 
-        private name: string;
-        public team: string;
-        public realName?: string;
+        //private name: string;
+        //public team: string;
+        //public realName?: string;
         static avgAge: number = 35;
+        static getAvgAge(){
+            return this.name;
+        }
 
-        constructor(name: string, team: string, realName?: string){
-            this.name = name;
-            this.team = team;
-            this.realName = realName
+        constructor(
+            private name: string, 
+            private team: string, 
+            public realName?: string,
+            avgAge: number = 55
+            ){
+                Avenger.avgAge = avgAge;
+            }
+
+        public bio(){
+            return `${ this.name } (${ this.team })`
         }
 
     }
 
-    const antman: Avenger = new Avenger('Antman','Capitan');
+    const antman: Avenger = new Avenger('Antman','Capitan', 'Scott Lang');
 
     console.log(antman)
-
+    console.log(Avenger.getAvgAge())
 
 })()
