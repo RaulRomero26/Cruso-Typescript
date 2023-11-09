4826a3886f17c11a482e1f4750efb2de2c705278
RaulRomero26 <link_2642@hotmail.com>
2023-11-09 09:48:25 -0600

    update: node con ts

---
+++ ts-node/src/index.ts
@@ -0,0 +1,11 @@
+// import { Hero as SuperHero, Hero2 } from "./classes/Hero"
+import * as HeroClasses from './classes/Hero'
+import powers from './data/powers';
+
+const Hero = 123;
+
+const ironman = new HeroClasses.Hero('Ironman',1,55)
+
+console.log(ironman.power)
+
+console.log(powers);
